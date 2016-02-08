eb583108b4d9cf51f36ca81117a56acdafd704ce
Ygor Santos <ygor.rodolfo.santos@ccc.ufcg.edu.br>
2016-02-08 14:23:01 -0300

    Implementação do get /users

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var bodyParser = require('body-parser');
 
 app.listen(5000);
 
-app.user(bodyParser.json());
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencode({
-	extend: true;
+app.use(bodyParser.urlencoded({
+	extend: true
 }));
 
 app.get('/' , function(req, res){
@@ -17,7 +17,11 @@ app.get('/' , function(req, res){
 });
 
 app.get('/users' , function(req, res){
-	
+	res.json([
+		{name: "Bulbasaur"},
+		{name: "Charmander"},
+		{name: "Squirtle"}
+	]);
 }); 
 
 app.get('/users/:id' , function(req, res){
@@ -26,7 +30,7 @@ app.get('/users/:id' , function(req, res){
 
 app.post('/users' , function(req, res){
 	res.end('post users');
-}); .gitignore
+});
 
 app.put('/users' , function(req, res){
 	res.end('put users');
